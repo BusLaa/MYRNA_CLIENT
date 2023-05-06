4213225fcf3d665dc804eb01d1528c642dffb2d6
MajorGerman <georgchannel8@gmail.com>
2023-05-06 19:17:47 +0300

    Image sending in addpost 1.0

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { gql } from 'graphql-request';
 import './AddPost.css';
 
@@ -13,10 +13,12 @@ function AddPost (props) {
     const [errorStyle, setErrorStyle] = useState("addPostError hidden");
     const [errorText, setErrorText] = useState("");
 
+    const [imageFile, setImageFile] = useState(null);
+
     async function addPost(e) {
         
 
-        if (header == "" || header.trim().slice(0, 1) == " " || content == "" || content.trim().slice(0, 1) == " ") {
+        if (header === "" || header.trim().slice(0, 1) === " " || content === "" || content.trim().slice(0, 1) === " ") {
             setErrorText("You need to fill all the fields!")
             setErrorStyle("addPostError");
             return;
@@ -36,7 +38,22 @@ function AddPost (props) {
         `; 
 
         try {
+            const data = new FormData();
+            data.append("image", imageFile);
+            fetch(process.env.REACT_APP_SERVER_IP + "upload", {
+                method: 'POST',
+                body: data
+            }).then((a) =>{
+                return a.json()
+            }).then((b) => {
+                console.log(b);
+            })
+        } catch (err) {
+            setErrorText(err)
+            setErrorStyle("logFormError");
+        }
 
+        try {
             return await fetch(process.env.REACT_APP_SERVER_IP, {
                 headers: {'Content-Type': 'application/json'},
                 method: 'POST',
@@ -45,7 +62,7 @@ function AddPost (props) {
                 return a.json()
             }).then((b) => {
                 window.location.href = "http://localhost:3000/allPosts";
-                return  b
+                return b
             })
 
         } catch (err) {
@@ -55,7 +72,14 @@ function AddPost (props) {
 
         }   
     }
-   
+
+    function updateImage() {
+        if (document.getElementById("image")) {
+            let files = document.getElementById("image")?.files;
+            setImageFile(files[0])
+        }
+    }
+
     return(
 
         <div className='addPostPage'>
@@ -64,7 +88,7 @@ function AddPost (props) {
 
                 <p className='addPostText'> Add a New Post </p> 
 
-                    <form method="POST" onSubmit={async (e) => {e.preventDefault(); addPost(e);}}>
+                    <form method="POST" enctype="multipart/form-data" onSubmit={async (e) => {e.preventDefault(); addPost(e);}}>
 
                         <div className={errorStyle}>
                             <p className="addPostErrorText">{errorText}</p>
@@ -72,15 +96,19 @@ function AddPost (props) {
 
                         <div className='addPostForm'>
                             
-
                             <div className='addPostFormText'>
                                 <textarea required onChange={(e) => {setHeader(e.target.value); console.log(e.target.value)}} onKeyDown={(e) => {if(e.keyCode === 13) { e.preventDefault();} }} maxLength="128" placeholder="My last dinner" name="header"></textarea>
                                 <textarea required onChange={(e) => {setContent(e.target.value)}} maxLength="4096" placeholder="This pie was so finger lickin' good..." name="content"></textarea>    
+                            </div>
+
+                            <div className='addPostFormImage'>
+                                <input multiple="multiple" maxLength={3} id="image" name="image" accept="image/png, image/jpeg" onChange={(e) => {updateImage(e)}} type="file"></input>
                             </div>
                             
                             <div className='addPostFormSubmit'>
                                 <input type="submit" value=" Here we go "></input>
                             </div>
+                            
 
                         </div>
 
